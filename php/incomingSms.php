1d19b8e2ab93057de9467da8e99ffb2008083c4c
Thompson David Naidu <thompsonnaiducm1343@gmail.com>
2017-10-26 18:47:28 +0530

    Update incomingSms.php

--- php/incomingSms.php
+++ php/incomingSms.php
@@ -1,4 +1,6 @@
 <?php
+
+//PHP CODE FOR EXOTEL
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
@@ -9,10 +11,9 @@
 	if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
 		exit();
 	}	
-
-	
-	//http://localhost:8080/incomingSms.php?SmsSid=132456&From=9052161119&To=56070&Date=12/2/2017&Body=Hello+World
 	//Extratct the GEt parameters from url 
+	//http://localhost:8080/incomingSms.php?SmsSid=132456&From=9052161119&To=56070&Date=12/2/2017&Body=Hello+World
+	
 	$SmsSid = $_GET["SmsSid"];
 	$From = $_GET["From"];
 	$To = $_GET["To"];
@@ -21,15 +22,19 @@
 
 
 
-	// Create connection
+	// Create connection to Database
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
+
+
+
+
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 
 	} 
 
-	//SQl query fro insertion of data
+	//SQl query from insertion of data
 	$sql = "INSERT INTO incomingsms(`smsSid`, `sender`, `receiver`, `date`, `body`) VALUES ('$SmsSid', '$From', '$To', $Date, '$Body')";
 
 	if ($conn->query($sql) === TRUE) {
